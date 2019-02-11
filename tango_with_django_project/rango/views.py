6ec80d8e3d395fd887f53aa9843839ed4092a22d
Barbara Sasfy <1106114s@student.gla.ac.uk>
2019-02-11 17:32:34 +0000

    End of Chapter 10 commit, missing informative error messages and number of visits.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -17,22 +17,42 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+from datetime import datetime
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visit', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days>0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    #return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
-
-    return render(request, 'rango/index.html', context_dict,)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    print(request.session['visits'])
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
     context_dict = {'boldmessage':   "This tutorial has been put together by Barbara!"}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    print(request.session['visits'])
     #return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
     print(request.method)
     print(request.user)
@@ -143,3 +163,9 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
